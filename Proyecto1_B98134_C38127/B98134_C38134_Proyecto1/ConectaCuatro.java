5eaeb0968f2f2eb30c3351866fc6b17209576595
LAPTOPCAROLA\Dell 7490s <vargasalfaro.caro@gmail.com>
2024-10-17 12:52:23 -0600

    Clases con doxygen

--- Proyecto1_B98134_C38127/B98134_C38134_Proyecto1/ConectaCuatro.java
+++ Proyecto1_B98134_C38127/B98134_C38134_Proyecto1/ConectaCuatro.java
@@ -1,27 +1,46 @@
 import java.util.Scanner;
 
+/**
+ * Clase ConectaCuatro.
+ * Esta clase representa el juego "Cuatro en Línea".
+ */
 public class ConectaCuatro {
-    private int jugadorActual; //jugador en un int ya que puede ser jugador1 o jugador 2
-    private char[][] tablero; //arreglo de arreglos para crear tablero
+    private int jugadorActual; ///< jugador en un int ya que puede ser jugador1 o jugador 2
+    private char[][] tablero; ///< arreglo de arreglos para crear tablero
 
-    public ConectaCuatro() { // constructor de clase
+    /**
+     * Constructor de la clase ConectaCuatro.
+     * Inicializa el tablero y llama al método iniciarJuego().
+     */
+    public ConectaCuatro() { 
         tablero = new char[7][6]; //tablero de 7 filas y 6 columnas
         iniciarJuego(); // método iniciar el juego
     }
-    
-    public int getJugadorActual(){ //get
+
+    /**
+     * Método get para obtener el jugador actual.
+     * @return jugadorActual El jugador actual (1 o 2).
+     */
+    public int getJugadorActual(){ 
         return jugadorActual;
     }
 
-    public void iniciarJuego() { // método para iniciar el juego
+    /**
+     * Método para iniciar el juego.
+     * Llena el tablero con puntos para representar espacios vacíos y asigna el turno al jugador 1.
+     */
+    public void iniciarJuego() { 
         for (int i = 0; i < 7; i++) { 
-            for (int j = 0; j < 6; j++) { //for para recorrer el tablero y poner un . para dar a entender que es un cuadro vacio
+            for (int j = 0; j < 6; j++) { //for para recorrer el tablero y poner un . para dar a entender que es un cuadro vacío
                 tablero[i][j] = '.'; // espacio vacío en el tablero para que se note donde no hay fichas
             }
         }
         jugadorActual = 1; // el jugador 1 es el que comienza jugando
     }
 
+    /**
+     * Muestra el tablero en la consola.
+     */
     public void mostrarTablero() {
         System.out.println("  1 2 3 4 5 6"); //mostrar los números de las columnas
         for (int i = 0; i < 7; i++) {
@@ -33,18 +52,23 @@ public class ConectaCuatro {
         }
     }
 
-    public boolean hacerMovimiento(int columna) { // método para realizar una jugada en el turno correspondiente
-        columna -= 1; //  la columna para que sea de 1-6 para el usuario
-        
-        if (columna < 0 || columna >= 6) { /* si el usuario escoje una columna que no existe (debajo de 0 o arriba de 6), se 
+    /**
+     * Método para realizar una jugada en la columna correspondiente.
+     * @param columna La columna en la que se quiere hacer el movimiento.
+     * @return true si el movimiento es válido, false si la columna no es válida o está llena.
+     */
+    public boolean hacerMovimiento(int columna) { 
+        columna -= 1; // la columna para que sea de 1-6 para el usuario
+
+        if (columna < 0 || columna >= 6) { /* si el usuario escoge una columna que no existe (debajo de 0 o arriba de 6), se 
             pide que digite otra*/ 
             System.out.println("La columna digitada no existe, elija otra entre 1 y 6");
             return false; // false porque no puede hacer el movimiento 
         }
-         for (int fila = 6; fila >= 0; fila--) { //for para ver cuál es la fila de más abajo sin llenar y la ficha va ahi 
-            if (tablero[fila][columna] == '.') { //si esa columna y fila está vacia entonces se puede poner la ficha del jugador
+        for (int fila = 6; fila >= 0; fila--) { //for para ver cuál es la fila de más abajo sin llenar y la ficha va ahí 
+            if (tablero[fila][columna] == '.') { //si esa columna y fila está vacía entonces se puede poner la ficha del jugador
                 tablero[fila][columna] = (jugadorActual == 1) ? 'X' : 'O'; //ya sea la ficha x || o
-                return true; // se usan operadores ternarios para hacer menos lineas de codigo
+                return true; // se usan operadores ternarios para hacer menos líneas de código
             }
         }
 
@@ -52,33 +76,41 @@ public class ConectaCuatro {
         return false;
     }
 
-    public boolean esJuegoTerminado() { // verifica que el juego terminó y devuelve si hay un ganador o ha sido un empate
-        return esGanador() || esEmpate(); // el empate solo sucede si ninguno de los dos jugadores gana, no pueden ganar lso dos
+    /**
+     * Verifica si el juego ha terminado, ya sea porque hay un ganador o porque hay un empate.
+     * @return true si el juego ha terminado, false en caso contrario.
+     */
+    public boolean esJuegoTerminado() { 
+        return esGanador() || esEmpate(); // el empate solo sucede si ninguno de los dos jugadores gana
     }
 
-    public boolean esGanador() { /* verifica si hay un ganador, esto cuando 4 fichas del mismo tipo se juntan/ Verifica filas, 
-        columnas y diagonales*/
-        for (int i = 0; i < 7; i++){ // para verificar fichas en la posicion horizontsl, vertical y diagonal
+    /**
+     * Verifica si hay un ganador.
+     * @return true si hay un ganador, false en caso contrario.
+     */
+    public boolean esGanador() { /* verifica si hay un ganador, esto sucede cuando 4 fichas del mismo tipo se juntan. Verifica filas, 
+        columnas y diagonales */
+        for (int i = 0; i < 7; i++){ // para verificar fichas en la posición horizontal, vertical y diagonal
             for (int j = 0; j < 6; j++) {
                 char ficha = tablero[i][j];
                 if (ficha == '.') continue;  // si no hay nada, se sigue
 
-                //verificar ficha juntas horizontalmente
+                // verificar fichas juntas horizontalmente
                 if (j + 3 < 6 && ficha == tablero[i][j + 1] && ficha == tablero[i][j + 2] && ficha == tablero[i][j + 3]) {
                     return true;
                 }
-                
-                //verifica fichas juntas verticalmente
+
+                // verificar fichas juntas verticalmente
                 if (i + 3 < 7 && ficha == tablero[i + 1][j] && ficha == tablero[i + 2][j] && ficha == tablero[i + 3][j]) {
                     return true;
                 }
-                
+
                 // verificar cuatro fichas juntas en diagonal derecha
                 if (i + 3 < 7 && j + 3 < 6 && ficha == tablero[i + 1][j + 1] && ficha == tablero[i + 2][j + 2] && ficha == tablero[i + 3][j + 3]) {
                     return true;
                 }
-                
-                // vreificar cuatro fichas juntas en diagonla izquierda
+
+                // verificar cuatro fichas juntas en diagonal izquierda
                 if (i + 3 < 7 && j - 3 >= 0 && ficha == tablero[i + 1][j - 1] && ficha == tablero[i + 2][j - 2] && ficha == tablero[i + 3][j - 3]) {
                     return true;
                 }
@@ -87,18 +119,25 @@ public class ConectaCuatro {
         return false;
     }
 
-    public boolean esEmpate() { //metodo que verifica si hay empate
+    /**
+     * Verifica si el juego ha terminado en empate.
+     * @return true si es empate, false si aún hay espacios vacíos.
+     */
+    public boolean esEmpate() { // método que verifica si hay empate
         for (int i = 0; i < 7; i++) {
-            for (int j = 0; j < 6; j++) { //recorre el array 
+            for (int j = 0; j < 6; j++) { // recorre el array
                 if (tablero[i][j] == '.') {
                     return false; // si aún queda un espacio vacío, no hay empate ya que hay posibilidad de ganar
                 }
             }
         }
-        return true; // si ya no quedan espacios vacíos, y nongún jugador ha ganado, es un empate
+        return true; // si ya no quedan espacios vacíos y ningún jugador ha ganado, es un empate
     }
-    
-    public void cambiarJugador() { //método que cambia el turno de los jugadores
+
+    /**
+     * Método que cambia el turno de los jugadores.
+     */
+    public void cambiarJugador() { 
         jugadorActual = (jugadorActual == 1) ? 2 : 1; // si el jugador actual es 1, se cambia a jugador 2 y viceversa
     }
 }
--- Proyecto1_B98134_C38127/B98134_C38134_Proyecto1/JuegoControlador.java
+++ Proyecto1_B98134_C38127/B98134_C38134_Proyecto1/JuegoControlador.java
@@ -1,115 +1,139 @@
 import java.util.Scanner;
-
+/**
+ * Clase que controla la selección y ejecución de los juegos TicTacToe y Conecta Cuatro.
+ */
 public class JuegoControlador {
-    private TicTacToe ticTacToe; // atributo objeto de la clase TicTacToe
-    private ConectaCuatro connectFour; // atrib objeto de la clase ConectaCuatro
-    private Object juegoActual; // objeto que referencia a TicTacToe o ConectaCuatro, según lo que se seleccione
+    private TicTacToe ticTacToe; ///< atributo objeto de la clase TicTacToe
+    private ConectaCuatro connectFour; ///< atrib objeto de la clase ConectaCuatro
+    private Object juegoActual; ///< objeto que referencia a TicTacToe o ConectaCuatro, según lo que se seleccione
 
-    public JuegoControlador() { //constructor de clase
+    /**
+     * Constructor de la clase JuegoControlador. Inicializa los objetos TicTacToe y ConectaCuatro.
+     */
+    public JuegoControlador() {
         ticTacToe = new TicTacToe(); 
         connectFour = new ConectaCuatro(); 
     }
     
-    public static void main(String[] args) {  //main 
+    /**
+     * Método principal que inicia la ejecución del programa.
+     * @param args Argumentos de línea de comandos.
+     */
+    public static void main(String[] args) {  
         JuegoControlador controlador = new JuegoControlador(); 
-        controlador.procesarEntradaUsuario(); //llama metodo procesarEntradaUsuario
+        controlador.procesarEntradaUsuario(); ///< llama metodo procesarEntradaUsuario
     }
 
-    public void mostrarMenu() { // mostar el menú para que el usuario seleccione el juego que desee
+    /**
+     * Muestra el menú para que el usuario seleccione el juego que desee.
+     */
+    public void mostrarMenu() {
         System.out.println("Hola, bienvenido. Seleccione un juego:"); 
-        System.out.println("1. TicTacToe"); // opción 1 para TicTacToe
-        System.out.println("2. Cuatro en Línea"); // opción 2 para Conecta Cuatro
+        System.out.println("1. TicTacToe"); ///< opción 1 para TicTacToe
+        System.out.println("2. Cuatro en Línea"); ///< opción 2 para Conecta Cuatro
     }
 
-    public void seleccionarJuego() { //permite al usuario seleccionar el juego que quiera
-        Scanner scanner = new Scanner(System.in); // entrada del usuario
-        mostrarMenu(); // se llama al método mostrarMenu para desplegar las opciones
-        int opcion = scanner.nextInt(); // leer entrada usuaria
+    /**
+     * Permite al usuario seleccionar el juego que quiera.
+     */
+    public void seleccionarJuego() {
+        Scanner scanner = new Scanner(System.in); ///< entrada del usuario
+        mostrarMenu(); ///< se llama al método mostrarMenu para desplegar las opciones
+        int opcion = scanner.nextInt(); ///< leer entrada usuaria
         
-        switch (opcion) { // se evalúa la opción seleccionada
-            case 1: // si elige la opción 1 se abre el tictactoe
+        switch (opcion) {
+            case 1: ///< si elige la opción 1 se abre el tictactoe
                 juegoActual = ticTacToe; 
                 System.out.println("Ha seleccionado el juego TicTacToe."); 
                 break;
-            case 2: // si elige la opción 2 se abre Conecta Cuatro
+            case 2: ///< si elige la opción 2 se abre Conecta Cuatro
                 juegoActual = connectFour; 
                 System.out.println("Ha seleccionado el juego Conecta Cuatro."); 
                 break;
-            default: // si se elige una opción no válida
+            default: ///< si se elige una opción no válida
                 System.out.println("Opción no válida. Seleccione otra vez por favor."); 
-                seleccionarJuego(); // se vuelve a llamar al método para que el usuario seleccione nuevamente una opcion válida
+                seleccionarJuego(); ///< se vuelve a llamar al método para que el usuario seleccione nuevamente una opcion válida
                 break;
         }
     }
 
-    public void jugar() { //identifica el juego que se eligió y lo corre
-        if (juegoActual instanceof TicTacToe) { // si juegoActual es una instancia de TicTacToe
-            jugarTicTacToe(); // se llama al método para jugar TicTacToe
-        } else if (juegoActual instanceof ConectaCuatro) { // si juegoActual es una instancia de ConectaCuatro
-            jugarConectaCuatro(); // llaamr al método para jugar Conecta Cuatro
+    /**
+     * Identifica el juego que se eligió y lo inicia.
+     */
+    public void jugar() {
+        if (juegoActual instanceof TicTacToe) { ///< si juegoActual es una instancia de TicTacToe
+            jugarTicTacToe(); ///< se llama al método para jugar TicTacToe
+        } else if (juegoActual instanceof ConectaCuatro) { ///< si juegoActual es una instancia de ConectaCuatro
+            jugarConectaCuatro(); ///< llamar al método para jugar Conecta Cuatro
         }
     }
 
-    public void jugarTicTacToe() { //ciclo del juego TicTacToe
-        Scanner scanner = new Scanner(System.in); //leer la entrada del usuario
-        while (!ticTacToe.esJuegoTerminado()) { // mientras el juego no haya terminado (no hay ganador ni empate)
-            ticTacToe.mostrarTablero(); // sigue mostrando el tablero
+    /**
+     * Ejecuta el ciclo del juego TicTacToe.
+     */
+    public void jugarTicTacToe() {
+        Scanner scanner = new Scanner(System.in); ///< leer la entrada del usuario
+        while (!ticTacToe.esJuegoTerminado()) { ///< mientras el juego no haya terminado (no hay ganador ni empate)
+            ticTacToe.mostrarTablero(); ///< sigue mostrando el tablero
             System.out.println("Jugador " + ticTacToe.getJugadorActual() + ", ingrese fila y columna (0-2):");
-            // pideal jugador actual que ingrese la fila y columna para hacer un movimiento
+            // pide al jugador actual que ingrese la fila y columna para hacer un movimiento
             int fila = scanner.nextInt(); 
             int columna = scanner.nextInt(); 
             
-            if (ticTacToe.hacerMovimiento(fila, columna)) { // si es un movimiento válido
-                if (ticTacToe.esGanador()) { // verifica si el jugador ha ganado
-                    ticTacToe.mostrarTablero(); // muestra el tablero final
+            if (ticTacToe.hacerMovimiento(fila, columna)) { ///< si es un movimiento válido
+                if (ticTacToe.esGanador()) { ///< verifica si el jugador ha ganado
+                    ticTacToe.mostrarTablero(); ///< muestra el tablero final
                     System.out.println("Jugador " + ticTacToe.getJugadorActual() + " ha ganado. ¡Felicidades!"); 
-                    
                     break; 
-                } else if (ticTacToe.esEmpate()) { // verifica si es un empate
-                    ticTacToe.mostrarTablero(); // muestra el tablero final
+                } else if (ticTacToe.esEmpate()) { ///< verifica si es un empate
+                    ticTacToe.mostrarTablero(); ///< muestra el tablero final
                     System.out.println("Ha sido un empate."); 
-                    
                     break; 
                 }
-                ticTacToe.cambiarJugador(); // cambia el turno al otro jugador
+                ticTacToe.cambiarJugador(); ///< cambia el turno al otro jugador
             } else { 
-                // en caso de que el el movimiento sea inválido
+                // en caso de que el movimiento sea inválido
                 System.out.println("Movimiento inválido, por favor intente de nuevo."); 
             }
         }
     }
 
-    public void jugarConectaCuatro() { //método que corre el juego conecta cuatro
-        Scanner scanner = new Scanner(System.in); // para leer la entrada del usuario
-        while (!connectFour.esJuegoTerminado()) { //mientars el juego no haya terminado (no hay ganador ni empate)
-            connectFour.mostrarTablero(); // muestra el tablero 
+    /**
+     * Ejecuta el ciclo del juego Conecta Cuatro.
+     */
+    public void jugarConectaCuatro() {
+        Scanner scanner = new Scanner(System.in); ///< para leer la entrada del usuario
+        while (!connectFour.esJuegoTerminado()) { ///< mientras el juego no haya terminado (no hay ganador ni empate)
+            connectFour.mostrarTablero(); ///< muestra el tablero 
             System.out.println("Jugador " + (connectFour.getJugadorActual()) + ", elija una columna (1-6):");
             // pide al jugador actual que elija una columna para hacer un movimiento
-            int columna = scanner.nextInt(); // leer columna ingresada por persona usuaria
+            int columna = scanner.nextInt(); ///< leer columna ingresada por persona usuaria
             
-            if (connectFour.hacerMovimiento(columna)) { // si es un movimiento es válido
-                if (connectFour.esGanador()) {  // revisa si el jugador ha ganado
-                    connectFour.mostrarTablero(); // muestra el tablero final
+            if (connectFour.hacerMovimiento(columna)) { ///< si es un movimiento válido
+                if (connectFour.esGanador()) {  ///< revisa si el jugador ha ganado
+                    connectFour.mostrarTablero(); ///< muestra el tablero final
                     System.out.println("Jugador " + connectFour.getJugadorActual() + " ha ganado- ¡Felicidades!"); 
                     break; 
                 
-                } else if (connectFour.esEmpate()) { // verifica si hay un empate
-                    connectFour.mostrarTablero(); // muestra el tablero final
+                } else if (connectFour.esEmpate()) { ///< verifica si hay un empate
+                    connectFour.mostrarTablero(); ///< muestra el tablero final
                     System.out.println("¡Es un empate!"); 
                     break;
                 }
-                connectFour.cambiarJugador(); // cambia el turno al otro jugador
+                connectFour.cambiarJugador(); ///< cambia el turno al otro jugador
             } else { 
-                // is el movimiento es inválido se pide otro intento
+                // si el movimiento es inválido se pide otro intento
                 System.out.println("Movimiento inválido, intente de nuevo.");
             }
-            
         }
     }
 
-    public void procesarEntradaUsuario() { // para leer y pre¿ocesar las entrdas de la persona usuaria
-        seleccionarJuego(); // llama al método para seleccionar el jugo
-        jugar(); // llama al método para iniciar el juego
+    /**
+     * Procesa las entradas de la persona usuaria y gestiona la selección del juego.
+     */
+    public void procesarEntradaUsuario() { 
+        seleccionarJuego(); ///< llama al método para seleccionar el juego
+        jugar(); ///< llama al método para iniciar el juego
     }
 
 }
